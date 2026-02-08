27d2b6c43b941b84d3335515eef234983640953c
EntropyXi <25804170@qq.com>
2026-02-08 16:05:59 +0800

    Site updated: 2026-02-08 16:05:59

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/02/08/hello-world/","2026/02/08/深度学习/RMSProp/","2026/02/08/深度学习/CNN/CNN的设计原则与数学推导/"];function toRandomPost(){
+var posts=["2026/02/08/hello-world/","2026/02/08/深度学习/CNN/CNN的设计原则与数学推导/","2026/02/08/深度学习/RMSProp/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
