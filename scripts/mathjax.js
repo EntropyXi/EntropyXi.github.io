80d5070e5f7a9c259cc003826897cd0e39de61df
EntropyXi <25804170@qq.com>
2026-02-08 17:22:26 +0800

    fix_latex_and_deploy

--- scripts/mathjax.js
+++ scripts/mathjax.js
@@ -1,15 +1,2 @@
 // D:\Blog_file\scripts\mathjax.js
-
-// 这是一个脚本注入器，它会自动把 MathJax 的代码插入到每个页面的底部
-hexo.extend.injector.register('body_end', `
-  <script src="//cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/MathJax.js?config=TeX-MML-AM_CHTML"></script>
-  <script>
-    MathJax.Hub.Config({
-      tex2jax: {
-        inlineMath: [ ['$','$'], ["\\(","\\)"] ],
-        processEscapes: true,
-        skipTags: ['script', 'noscript', 'style', 'textarea', 'pre', 'code']
-      }
-    });
-  </script>
-`);
+// 这个脚本已被禁用，改用 hexo-filter-mathjax 插件，它能更好地处理 Markdown 标签冲突。
